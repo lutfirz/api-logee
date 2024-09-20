61fb09963232f0ce5a1f168ae04471fda4cec993
lutfirz <71687928+lutfirz@users.noreply.github.com>
2024-09-20 12:44:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-// rincian pembayaran
-// rute
-
 const express = require('express');
 const mongoose = require('mongoose');
-const multer = require('multer');
-const { createClient } = require('@supabase/supabase-js');
-const fs = require('fs');
-const path = require('path');
-const cors = require('cors');
-
-const upload = multer({ dest: 'uploads/' });
 const app = express();
 app.use(express.json());
-app.use(cors());
-
-const supabaseUrl = 'https://jyeyttpoogzirkarinyu.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZXl0dHBvb2d6aXJrYXJpbnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY3NTY2NDMsImV4cCI6MjA0MjMzMjY0M30.NER5Z0LDqPSHqGnyPB-7G8sXnx1gII5rPDuV2sH_NmE';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 mongoose
   .connect(
     'mongodb://lutfirazan:123@logee-shard-00-00.iowvi.mongodb.net:27017,logee-shard-00-01.iowvi.mongodb.net:27017,logee-shard-00-02.iowvi.mongodb.net:27017/?replicaSet=atlas-x7eqsc-shard-0&ssl=true&authSource=admin'
   )
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Failed to connect to MongoDB', err));
-
 const catalogSchema = new mongoose.Schema({
   origin: { type: String, required: true },
   destination: { type: String, required: true },
@@ -42,44 +24,7 @@ const catalogSchema = new mongoose.Schema({
   driver: [String],
   price: { type: Number, required: true },
 });
-
 const Catalog = mongoose.model('Catalog', catalogSchema);
-
-app.post('/upload', upload.single('image'), async (req, res) => {
-  const file = req.file;
-
-  try {
-    // Read the file from the file system
-    const filePath = path.join(__dirname, file.path);
-    const fileBuffer = fs.readFileSync(filePath);
-
-    // Upload the file to Supabase storage bucket
-    const { data, error } = await supabase.storage
-      .from('image') // Use the bucket you created
-      .upload(`images/${file.filename}-${file.originalname}`, fileBuffer, {
-        contentType: file.mimetype,
-        cacheControl: '3600',
-        upsert: false,
-      });
-
-    // Clean up the local file after upload
-    fs.unlinkSync(filePath);
-
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-
-    // Get the public URL of the uploaded image
-    const { publicURL } = supabase.storage
-      .from('image')
-      .getPublicUrl(`images/${file.filename}-${file.originalname}`);
-
-    res.json({ message: 'File uploaded successfully', url: publicURL });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 app.post('/catalog', async (req, res) => {
   try {
     const catalogData = req.body;
@@ -96,13 +41,10 @@ app.post('/catalog', async (req, res) => {
     });
   }
 });
-
 app.get('/catalogs', async (req, res) => {
-  const catalogs = await Catalog.find(req.query);
-  console.log(catalogs);
+  const catalogs = await Catalog.find();
   res.json(catalogs);
 });
-
 app.get('/catalogs/:id', async (req, res) => {
   const _id = req.params.id;
   const catalog = await Catalog.findOne({ _id });
@@ -111,7 +53,6 @@ app.get('/catalogs/:id', async (req, res) => {
   }
   res.json(catalog);
 });
-
 app.listen(3000, () => {
   console.log('Server running on http://127.0.0.1:3000');
 });
