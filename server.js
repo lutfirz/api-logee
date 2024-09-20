b690b53a9f98928116234f7bdcd5ca1c4502c92e
lutfirz <71687928+lutfirz@users.noreply.github.com>
2024-09-20 12:33:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+const cors = require('cors');
 
 mongoose
   .connect(
